9c8c8b8d96c3dc760b24cfc0023fa7ae5d2ab058
TsungYu-Chen <bleach1395@gmail.com>
2016-04-07 23:19:53 +0800

    Update MyBubbleSort.java

--- MyBubbleSort.java
+++ MyBubbleSort.java
@@ -1,3 +1,5 @@
+//U10416018 陳宗佑
+
 import java.util.Scanner;
 
 public class MyBubbleSort{
@@ -5,13 +7,15 @@ public class MyBubbleSort{
 		boolean needNextPass = true;
 		
 		for(int k = 1; k < list.length && needNextPass; k++){
+			//Array may be sorted and next pass not needed
 			needNextPass = false;
 			for(int i = 0; i < list.length - k; i++){
 				if(list[i] > list[i + 1]){
+					//Swap list[i] with list[i + 1]
 					int temp = list[i];
 					list[i] = list[i + 1];
 					list[i + 1] = temp;
-					
+					//Next pass still needed
 					needNextPass = true;
 				}
 			}
@@ -20,15 +24,17 @@ public class MyBubbleSort{
 	
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
+		//Ask how many numbers does user want to enter
 		System.out.println("想輸入幾個數字?");
 		int num = input.nextInt();
 		int[] list = new int[num];
-		
+		//Enter numbers
 		System.out.println("請輸入 " + num + " 個數字 :");
 		for(int i = 0; i < num; i++){
-			list[i] =  input.nextInt();
-			bubbleSort(list);
+			list[i] = input.nextInt();
 		}
+		//Add the numbers to bubbleSort(int[] list)
+		bubbleSort(list);
 		for(int i = 0; i < list.length; i++){
 			System.out.print(list[i] + " ");
 		}
